95fb0c6943755175221c365956c8e4a0ac9dbccc
Teodor Beles <33461406+damianbeles@users.noreply.github.com>
2018-04-12 11:55:28 +0300

    Added ToString() method

--- StoreManager/StoreManager/DateTime.cpp
+++ StoreManager/StoreManager/DateTime.cpp
@@ -55,3 +55,7 @@ void DateTime::setYear(int year) {
 		year_ = year;
 	else throw DateTimeError::YEAR;
 }
+
+std::string DateTime::ToString() const {
+	return std::to_string(hour_) + ":" + std::to_string(minute_) + ":" + std::to_string(second_) + " " + std::to_string(day_) + "/" + std::to_string(month_) + "/" + std::to_string(year_);
+}
